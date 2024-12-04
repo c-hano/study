68574c9fde06789e579781fcf400524b61c9d5d3
c-hano <wdq0304@naver.com>
2024-12-04 15:06:05 +0900

    RefToInt

---
+++ src/stream/RefToInt.java
@@ -0,0 +1,47 @@
+package stream;
+
+import java.util.Arrays;
+
+public class RefToInt {
+
+    public static void main(String[] args) {
+
+        System.out.println("== No Stream ==");
+        noStreamVersion();
+
+        System.out.println("== Stream ==");
+        streamVersion();
+
+    }
+
+    private static void noStreamVersion() {
+
+        String[] messages = {"1번", "2번", "3번"};
+        int[] numbers = new int[messages.length];
+
+        for (int i = 0; i < messages.length; i++) {
+            String message = messages[i];
+            message = message.substring(0, message.length() - 1);
+            int number = Integer.parseInt(message);
+
+            if (number % 2 == 0) continue;
+
+            numbers[i] = number;
+        }
+        System.out.println("= 결과 =");
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    private static void streamVersion() {
+
+        int[] array = Arrays.stream(new String[]{"1번", "2번", "3번"})
+                .map(e -> e.substring(0, e.length() - 1))
+                .mapToInt(Integer::parseInt)
+                .filter(number -> number % 2 != 0)
+                .toArray();
+
+        System.out.println("= 결과 =");
+        System.out.println(Arrays.toString(array));
+    }
+}
+
