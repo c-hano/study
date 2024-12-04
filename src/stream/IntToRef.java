ea40f33c04871c558c35911848a5e5bc7db3c5fd
chanho <wdq0304@gmail.com>
2024-12-04 14:26:47 +0900

    IntToRef

---
+++ src/stream/IntToRef.java
@@ -0,0 +1,53 @@
+package stream;
+
+import java.util.Arrays;
+
+public class IntToRef {
+    public static void main(String[] args) {
+        System.out.println("== No Stream ==");
+        noStreamVersion();
+
+        System.out.println("== Stream v1 ==");
+        streamVersionV1();
+
+        System.out.println("== Stream v2 ==");
+        streamVersionV2();
+    }
+
+    private static void noStreamVersion() {
+        int[] numbers = new int[]{1, 2, 3};
+
+        String[] messages = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) continue;
+
+            messages[i] = numbers[i] + "번";
+        }
+
+        System.out.println("= 원본 =");
+        System.out.println(Arrays.toString(numbers));
+        System.out.println("= 결과 =");
+        System.out.println(Arrays.toString(messages));
+    }
+
+    private static void streamVersionV1() {
+        Object[] array = Arrays.stream(new int[]{1, 2, 3})
+                .filter(e -> e % 2 != 0)
+                .mapToObj(e -> e + "번")
+                .toArray();
+
+        System.out.println("= 결과 =");
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void streamVersionV2() {
+        String[] array = Arrays.stream(new int[]{1, 2, 3})
+                .filter(e -> e % 2 != 0)
+                .mapToObj(e -> e + "번")
+                .toArray(String[]::new);
+                //.toArray(String[]::new) -> .toArray(size -> new String[size]) 와 동일
+        System.out.println("= 결과 =");
+        System.out.println(Arrays.toString(array));
+    }
+}
